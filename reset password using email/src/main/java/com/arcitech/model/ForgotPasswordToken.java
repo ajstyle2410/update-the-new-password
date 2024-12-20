f88fb9951fd5a0e018976d2ea8cbc29864bdfc8e
Ajay Sambhaji Deshmukh <78377310+ajstyle2410@users.noreply.github.com>
2024-12-20 09:52:26 +0530

    create the reset password page

---
+++ reset password using email/src/main/java/com/arcitech/model/ForgotPasswordToken.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2024, Arc-i-Tech. All rights reserved.
+ * visit www.arc-i-tech.in
+ *
+ */
+package com.arcitech.model;
+
+
+import javax.persistence.*;
+
+@Entity
+public class ForgotPasswordToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
